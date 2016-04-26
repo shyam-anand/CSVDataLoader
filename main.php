15517ad89f7be19b9713598cb63f4b211430af51
Shyam Anand <shyamwdr@gmail.com>
2016-04-26 10:39:33 +0530

    Initial commit

--- main.php
+++ main.php
@@ -1,6 +1,12 @@
 <?php
 $cmd_opts = getopt("f:t:s:d:u:p::h:", array("invalid-rows-file:","dry"));
 
+$mandatory_opts = array('f', 't', 'd', 'u', 'p', 'h');
+$cmd_opts_keys = array_keys($cmd_opts);
+if (count(array_intersect($cmd_opts_keys, $mandatory_opts)) != count($mandatory_opts)) {
+    die("Usage: php main.php -f source_file -t table_name -d db_name -h db_host -u db_user -p [db_password] [--invalid-rows-file invalid_rows_filename] [--dry dry_run]");
+}
+
 $source = $cmd_opts['f'];
 $table = $cmd_opts['t'];
 $dbname = $cmd_opts['d'];
